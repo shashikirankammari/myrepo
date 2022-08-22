ed4535c5a7663b78fb4748f76cb93ba6035f037c
shashikirankammari <111746249+shashikirankammari@users.noreply.github.com>
2022-08-22 16:47:36 +0530

    Add files via upload

---
+++ Myfirstclass.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class Myfirstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi friends!!!");
+		
+		System.out.print("hello world");
+
+
+	}
+
+}
